5dab1cfd2c1300b82f16a5cab32887046a2d7dfd
MukeshM-K <technicalmk108@gmail.com>
2024-03-08 12:52:30 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Generative-AI-Class-Work" 
---
+++ inquirer/main.js
@@ -0,0 +1,8 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let answer = await inquirer.prompt({
+    name: "age",
+    type: "number",
+    message: "Enter your age",
+});
+console.log(chalk.green("Insha Allah, in " + (60 - answer.age) + " years you will be 60 years old"));
