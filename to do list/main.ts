91d9f2c701a5a0a2c0e8e6584f24b1d9525b6fdf
MukeshM-K <technicalmk108@gmail.com>
2024-04-02 10:40:14 +0500

    first commit

---
+++ to do list/main.ts
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+
+let todos = [];
+let condition = true;
+
+
+while(condition){
+let addTask =  await inquirer.prompt([{
+    name: "todo",
+    type: "input",
+    message: "What you want to add in your Todos"
+},
+
+{
+    name: "addMore",
+    type: "confirm",
+    message: "Do you want to add more",
+    default: "false"
+}
+
+
+
+]);
+
+todos.push(addTask.todo)
+condition = addTask.addMore
+console.log(todos);
+
+}
